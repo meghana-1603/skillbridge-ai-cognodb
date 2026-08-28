aefa3b45dff473f13daf49273c79690cc8c049c1
meghana-1603 <dasarimeghana162006@gmail.com>
2026-08-28 13:24:53 +0530

    Fix Streamlit secrets support

--- db/connection.py
+++ db/connection.py
@@ -6,10 +6,9 @@ load_dotenv()
 
 try:
     import streamlit as st
-
-    URI = st.secrets.get("DB_URI", os.getenv("DB_URI"))
-    USER = st.secrets.get("DB_USER", os.getenv("DB_USER"))
-    PASSWORD = st.secrets.get("DB_PASSWORD", os.getenv("DB_PASSWORD"))
+    URI = st.secrets["DB_URI"]
+    USER = st.secrets["DB_USER"]
+    PASSWORD = st.secrets["DB_PASSWORD"]
 except Exception:
     URI = os.getenv("DB_URI")
     USER = os.getenv("DB_USER")
